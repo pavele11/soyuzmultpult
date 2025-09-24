b479766367c85bdacd2c42a0c09975a752773f98
Oxygenchick <43747014+pavele11@users.noreply.github.com>
2025-09-24 15:58:47 +0300

    апдейт

--- index.js
+++ index.js
@@ -48,6 +48,6 @@ const server = http.createServer((req, res) => {
   });
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Listening on ${PORT}`);
 });
